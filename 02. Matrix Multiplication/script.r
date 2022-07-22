8dcf8a209df294843e475bfa01b45c86a117beb0
asaldelkhosh <asaldelkhosh@gmail.com>
2022-07-22 20:02:08 +0430

    fix: matrix multiplication

--- 02. Matrix Multiplication/script.r
+++ 02. Matrix Multiplication/script.r
@@ -2,14 +2,14 @@
 
 # Creating matrices
 m <- matrix(1:8, nrow=2)
-n <- matrix(8:15, nrow=2)
+n <- matrix(8:15, nrow=4)
 
 # showing our matrixes
 print(m)
 print(n)
 
 # multiplying matrices
-r <- m*n
+r <- m %*% n
 
 # result
 print(r)
