297d1e7b357d42ef4e5fc4a7ef548bfc35e4b06d
asaldelkhosh <asaldelkhosh@gmail.com>
2022-07-25 09:57:11 +0430

    feat: readfile function

--- 05. Read all files/script.r
+++ 05. Read all files/script.r
@@ -1,11 +1,16 @@
-path <- "./05. Read all files/"
-pattern <- "*"
+# this function reads all the files in a path
+# that matches the givin pattern.
+read_files <- function(path=".", pattern="*") {
+    return (list.files(
+        path=path, 
+        pattern=pattern, 
+        full.names=TRUE
+    ))
+}
 
-files <- list.files(
-    path=path, 
-    pattern=pattern, 
-	full.names=TRUE
-)
+
+path <- "./05. Read all files/"
+files <- read_files(path)
 
 print(length(files))
 print(files)
